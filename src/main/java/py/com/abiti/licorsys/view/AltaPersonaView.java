b47a4456ac7e1ea75071c439864442dfcfd029b5
Matistack <78389209+Matistack@users.noreply.github.com>
2021-05-27 19:18:47 -0400

    ala persona
    formatee los campos, los ordené

--- src/main/java/py/com/abiti/licorsys/view/AltaPersonaView.java
+++ src/main/java/py/com/abiti/licorsys/view/AltaPersonaView.java
@@ -1,8 +1,13 @@
 package py.com.abiti.licorsys.view;
 
+import org.w3c.dom.Text;
+
 import com.vaadin.navigator.View;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.DateField;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 
@@ -14,11 +19,20 @@ public class AltaPersonaView extends CustomComponent implements View {
 	
 	private VerticalLayout mainLayout;
 	
+	private HorizontalLayout BotonLayout;
+	
 	private Button btnGuardar; 
-	private TextField txtNombre;
 	private Button btnSalir;
+	private Button btnAdd;
 	
-	
+	private TextField txtNombre;
+	private TextField txtApellido;
+	private TextField txtDireccion;
+	private TextField txtNroDoc;
+	private TextField txtRuc;
+	private TextField txtTelefono;
+	private DateField dfFecNac;
+	private ComboBox  cbxGenero;
 	
 	
 	
@@ -29,12 +43,72 @@ public class AltaPersonaView extends CustomComponent implements View {
 	public AltaPersonaView() {
 		
 		mainLayout = new VerticalLayout();
+		BotonLayout = new HorizontalLayout();
 		
 		setCompositionRoot(mainLayout);
 		
+		crearComponentes();
+		
+		mainLayout.addComponent(txtNombre);
+		
+		
+		
+		
+		mainLayout.addComponents(txtNombre);
+		mainLayout.addComponent(txtApellido);
+		mainLayout.addComponent(txtDireccion);
+		mainLayout.addComponent(txtNroDoc);
+		mainLayout.addComponent(txtRuc);
+		mainLayout.addComponent(txtTelefono);
+		mainLayout.addComponent(dfFecNac);
+		mainLayout.addComponent(cbxGenero);
+		
+		
+		mainLayout.addComponent(btnGuardar);
+		mainLayout.addComponent(btnSalir);
+		mainLayout.addComponent(btnAdd);
+		
+		mainLayout.addComponent(BotonLayout); 
+		BotonLayout.addComponents(btnSalir, btnAdd);
+		
+		
+		
+		
+	}
+
+
+
+
+
+
+
+	private void crearComponentes() {
+
 		txtNombre = new TextField();
+		txtNombre.setCaption("NOMBRE");
+		txtApellido = new TextField();
+		txtApellido.setCaption("APELLIDO");
+		txtDireccion = new TextField();
+		txtDireccion.setCaption("DIRECCION");
+		txtNroDoc = new TextField();
+		txtNroDoc.setCaption("NUMERO DOCUMENTO");
+		txtRuc = new TextField();
+		txtRuc.setCaption("RUC");
+		txtTelefono = new TextField();
+		txtTelefono.setCaption("TELEFONO");
+		dfFecNac = new DateField();
+		dfFecNac.setCaption("FECHA NACIMIENTO");
+		cbxGenero = new ComboBox();
+		cbxGenero.setCaption("GENERO");
+		
+		
+		
 		btnSalir = new Button();
-		btnSalir.setCaption("saliforro");
+		btnSalir.setCaption("salir");
+		btnAdd = new Button();
+		
+		
+		btnAdd.setCaption("Add");
 	
 		
 		
@@ -43,10 +117,8 @@ public class AltaPersonaView extends CustomComponent implements View {
 		btnGuardar = new Button();
 		btnGuardar.setCaption("Guardar");
 		
-		mainLayout.addComponent(btnGuardar);
-		mainLayout.addComponent(txtNombre);
-		mainLayout.addComponent(btnSalir);
 		
+
 		
 	}
 	
